5ed4368edfa783192a3a108e250f702355b26716
Atharva Kusumbia <atharvakusumbia@gmail.com>
2026-01-04 03:43:54 +0530

    Bump version to 0.1.1

--- engine/include/vayu/version.hpp
+++ engine/include/vayu/version.hpp
@@ -7,9 +7,9 @@
 
 #define VAYU_VERSION_MAJOR 0
 #define VAYU_VERSION_MINOR 1
-#define VAYU_VERSION_PATCH 0
+#define VAYU_VERSION_PATCH 1
 
-#define VAYU_VERSION_STRING "0.1.0"
+#define VAYU_VERSION_STRING "0.1.1"
 
 namespace vayu {
 
