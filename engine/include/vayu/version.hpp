6b5cc15a2fd250d064042ac3f2b5e3d6c95fe212
Atharva Kusumbia <atharvakusumbia@gmail.com>
2026-08-03 23:05:48 +0530

    chore(release): bump version to 0.15.0

--- engine/include/vayu/version.hpp
+++ engine/include/vayu/version.hpp
@@ -13,10 +13,10 @@
  */
 
 #define VAYU_VERSION_MAJOR 0
-#define VAYU_VERSION_MINOR 14
+#define VAYU_VERSION_MINOR 15
 #define VAYU_VERSION_PATCH 0
 
-#define VAYU_VERSION_STRING "0.14.0"
+#define VAYU_VERSION_STRING "0.15.0"
 
 namespace vayu {
 
