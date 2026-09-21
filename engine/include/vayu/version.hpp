6a9fb1959aff34f021ac7e5ad33b9f5687eeca56
Atharva Kusumbia <atharvakusumbia@gmail.com>
2026-09-21 18:45:51 +0000

    chore(release): 0.35.0

--- engine/include/vayu/version.hpp
+++ engine/include/vayu/version.hpp
@@ -18,11 +18,11 @@
 // spelling beside it.
 // NOLINTBEGIN(modernize-macro-to-enum)
 #define VAYU_VERSION_MAJOR 0
-#define VAYU_VERSION_MINOR 34
+#define VAYU_VERSION_MINOR 35
 #define VAYU_VERSION_PATCH 0
 // NOLINTEND(modernize-macro-to-enum)
 
-#define VAYU_VERSION_STRING "0.34.0"
+#define VAYU_VERSION_STRING "0.35.0"
 
 namespace vayu {
 
